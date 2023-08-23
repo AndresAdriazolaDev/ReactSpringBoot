b06cad38431926528dfc4c01ec5d66fc8105d9e5
Andres Adriazola <andres.adriazola.dev@outlook.com>
2023-08-23 00:57:23 -0400

    Counter App

---
+++ counter-app/src/CounterApp.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export const CounterApp = ({value}) => {
+  const [counter, setCounter] = useState(value);
+  const counterIncremet = () => setCounter(counter + 1);
+
+  return (
+    <>
+      <h2>El valor del contador es {counter} </h2>;
+      <button onClick={() => counterIncremet()}>Increment Counter +1</button>
+    </>
+  );
+};
